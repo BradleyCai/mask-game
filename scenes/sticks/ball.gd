91e9fb51de567679958c5617a2f19cc575e09e29
Craig Wilkinson <craig@crwi.uk>
2026-02-01 13:22:17 -0800

    Ball room improvements

--- scenes/sticks/ball.gd
+++ scenes/sticks/ball.gd
@@ -1,21 +1,7 @@
 extends Node3D
 
-@export var amplitude := 4.0
-@export var speed := 1.0
-
-var time := 0.0
-var start_z := 0.0
-var start_x := 0.0
-
 func _ready():
-	start_z = global_position.z
-	start_x = global_position.x
-
+	pass
 
 func _process(delta):
-	time += delta
-	var pos = global_position
-	pos.z = start_z + sin(time * speed) * amplitude
-	pos.x = start_x + cos(time * speed) * amplitude
-
-	global_position = pos
+	pass
--- scenes/sticks/sticks_2.gd
+++ scenes/sticks/sticks_2.gd
@@ -6,6 +6,7 @@ extends Node3D
 
 @export var smooth_speed := 8.0
 
+@onready var actual_pos: Vector3 = get_node(ball_path).global_position
 @onready var mask_pos: Node3D = get_node(ball_path)
 var bodies: Array[AnimatableBody3D] = []
 
@@ -14,15 +15,19 @@ func _ready():
 		if c is AnimatableBody3D:
 			bodies.append(c)
 
-func _process(delta):
-	var ball_pos = mask_pos.global_position
+func _physics_process(delta):
+	var target_pos = mask_pos.global_position
+	actual_pos = actual_pos.move_toward(target_pos, 3 * delta)
 
 	for body in bodies:
-		var d = body.global_position.distance_to(ball_pos)
+		var p1 = body.global_position
+		var p2 = actual_pos
+		p1.y = 0
+		p2.y = 0
+		var d = p1.distance_to(p2)
 
 		var curve_value = falloff.sample(d) # 0–1
-		var target_y = curve_value
 
 		var pos = body.global_position
-		pos.y = target_y
+		pos.y = curve_value
 		body.global_position = pos
