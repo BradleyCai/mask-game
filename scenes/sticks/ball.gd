cfc62babc2b705ce2e3f165919eda7aa140dce2e
Craig Wilkinson <craig@crwi.uk>
2026-01-31 19:41:08 -0800

    Ball room update with more sticks

--- scenes/sticks/ball.gd
+++ scenes/sticks/ball.gd
@@ -5,12 +5,17 @@ extends Node3D
 
 var time := 0.0
 var start_z := 0.0
+var start_x := 0.0
 
 func _ready():
 	start_z = global_position.z
+	start_x = global_position.x
+
 
 func _process(delta):
 	time += delta
 	var pos = global_position
 	pos.z = start_z + sin(time * speed) * amplitude
+	pos.x = start_x + cos(time * speed) * amplitude
+
 	global_position = pos
